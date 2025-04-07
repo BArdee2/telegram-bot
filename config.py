0fd86edb72f970ee9a8041d582f10dc98be44817
BArdee2 <chidoesiegwuemmanuel@gmail.com>
2025-04-07 09:21:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
     ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',') if id]
-    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///tasks.db')
+    DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost/database_name')
     MIN_WITHDRAWAL = float(os.getenv('MIN_WITHDRAWAL', '10.00'))
